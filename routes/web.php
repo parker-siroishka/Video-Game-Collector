c095e7a7164f7eb2de4c3a5978d124edf6a73c97
Parker Siroishka <parkersir10@hotmail.com>
2024-06-20 11:16:29 -0600

    Remove boilerplate welcome page and route

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Dashboard/Dashboard', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
