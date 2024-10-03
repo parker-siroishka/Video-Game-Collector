5b295e1ba6dc5770e025cee4db6ad41f445489ea
Parker Siroishka <parkersir10@hotmail.com>
2024-10-03 11:42:07 -0600

    Update more css to dark theme

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -25,11 +25,9 @@ const form = useForm({
 <template>
     <section>
         <header>
-            <h2 class="text-lg font-medium text-gray-900">
-                Profile Information
-            </h2>
+            <h2 class="text-lg font-medium text-white">Profile Information</h2>
 
-            <p class="mt-1 text-sm text-gray-600">
+            <p class="mt-1 text-sm text-white">
                 Update your account's profile information and email address.
             </p>
         </header>
