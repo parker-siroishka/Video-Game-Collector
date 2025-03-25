a96047d2faeedb2f7b7dd5f893553114ba4db760
Parker Siroishka <parkersir10@hotmail.com>
2025-03-25 15:58:00 -0600

    Only hide recently played CTAs on desktop

--- resources/js/Pages/Dashboard/Partials/RecentlyPlayedTile.vue
+++ resources/js/Pages/Dashboard/Partials/RecentlyPlayedTile.vue
@@ -44,7 +44,7 @@ onMounted(() => updateArt());
                 {{ gameTitle }}
             </p>
             <div
-                class="px-2 py-2 w-auto absolute bottom-0 mb-2 ml-2 flex space-x-3 bg-gray-700 bg-opacity-90 rounded-lg hidden group-hover:block"
+                class="px-2 py-2 w-auto absolute bottom-0 mb-2 ml-2 flex space-x-3 bg-gray-700 bg-opacity-90 rounded-lg md:hidden md:group-hover:block"
             >
                 <button
                     class="z-10 relative h-[34px] inline-flex items-center px-2 py-2 bg-orange-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase disabled:opacity-50 tracking-widest hover:bg-orange-600 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
